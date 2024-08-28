8bee5c54172b99c3e49c31ed094d31fc1ce27d63
bamideletewogbade <bishoptewogbade.com>
2024-08-28 00:12:50 +0000

    latest

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/bishoptewogbade/genaiq/venv"
+set -gx VIRTUAL_ENV "/home/bamidele_tewogbade1/genaiq/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
