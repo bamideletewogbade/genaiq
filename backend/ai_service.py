8cda3fc7802858f77945184932795ac96a6169f8
bamideletewogbade <YOUR_EMAIL_ADDRESS>
2024-09-02 14:34:14 +0000

    Feedback page generated successfully and page formatted

--- backend/ai_service.py
+++ backend/ai_service.py
@@ -6,20 +6,19 @@ import docx
 from io import BytesIO
 import os
 import logging
-import pdfkit
 import json
-import re
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 def initialize_vertex_ai(project_id, location):
     """Initializes the Vertex AI environment."""
     try:
         vertexai.init(project=project_id, location=location)
-        logging.info("Vertex AI initialized successfully.")
+        logger.info("Vertex AI initialized successfully.")
     except Exception as e:
-        logging.error(f"Failed to initialize Vertex AI: {e}")
+        logger.error(f"Failed to initialize Vertex AI: {e}")
         raise
 
 def download_file_from_gcs(file_uri):
@@ -30,10 +29,10 @@ def download_file_from_gcs(file_uri):
         bucket = storage_client.bucket(bucket_name)
         blob = bucket.blob(blob_name)
         file_content = blob.download_as_bytes()
-        logging.info(f"File downloaded successfully from GCS: {file_uri}")
+        logger.info(f"File downloaded successfully from GCS: {file_uri}")
         return file_content
     except Exception as e:
-        logging.error(f"Error downloading from GCS: {e}")
+        logger.error(f"Error downloading from GCS: {e}")
         return None
 
 def upload_local_file_to_gcs(file_path, bucket_name, destination_blob_name):
@@ -43,10 +42,10 @@ def upload_local_file_to_gcs(file_path, bucket_name, destination_blob_name):
         bucket = storage_client.bucket(bucket_name)
         blob = bucket.blob(destination_blob_name)
         blob.upload_from_filename(file_path)
-        logging.info(f"File uploaded successfully to GCS: {destination_blob_name}")
+        logger.info(f"File uploaded successfully to GCS: {destination_blob_name}")
         return f"gs://{bucket_name}/{destination_blob_name}"
     except Exception as e:
-        logging.error(f"Error uploading to GCS: {e}")
+        logger.error(f"Error uploading to GCS: {e}")
         return None
 
 def extract_text_from_pdf(file_content):
@@ -57,10 +56,10 @@ def extract_text_from_pdf(file_content):
             text = ""
             for page in reader.pages:
                 text += page.extract_text() or ""
-        logging.info("Text extracted successfully from PDF.")
+        logger.info("Text extracted successfully from PDF.")
         return text
     except Exception as e:
-        logging.error(f"Error extracting text from PDF: {e}")
+        logger.error(f"Error extracting text from PDF: {e}")
         return ""
 
 def extract_text_from_docx(file_content):
@@ -69,20 +68,20 @@ def extract_text_from_docx(file_content):
         with BytesIO(file_content) as file:
             doc = docx.Document(file)
             text = "\n".join(paragraph.text for paragraph in doc.paragraphs)
-        logging.info("Text extracted successfully from DOCX.")
+        logger.info("Text extracted successfully from DOCX.")
         return text
     except Exception as e:
-        logging.error(f"Error extracting text from DOCX: {e}")
+        logger.error(f"Error extracting text from DOCX: {e}")
         return ""
 
 def extract_text_from_txt(file_content):
     """Extracts text from a TXT file."""
     try:
         text = file_content.decode('utf-8')
-        logging.info("Text extracted successfully from TXT.")
+        logger.info("Text extracted successfully from TXT.")
         return text
     except Exception as e:
-        logging.error(f"Error extracting text from TXT: {e}")
+        logger.error(f"Error extracting text from TXT: {e}")
         return ""
 
 def call_vertex_ai(file_uri):
@@ -90,7 +89,7 @@ def call_vertex_ai(file_uri):
     file_content = download_file_from_gcs(file_uri)
     
     if not file_content:
-        logging.error(f"Failed to download file from GCS: {file_uri}")
+        logger.error(f"Failed to download file from GCS: {file_uri}")
         return {"error": "Failed to download file from GCS."}
     
     file_extension = os.path.splitext(file_uri)[1].lower()
@@ -102,31 +101,30 @@ def call_vertex_ai(file_uri):
     elif file_extension == '.txt':
         file_text = extract_text_from_txt(file_content)
     else:
-        logging.warning(f"Unsupported file type: {file_extension}")
+        logger.warning(f"Unsupported file type: {file_extension}")
         return {"error": "Unsupported file type."}
     
     if not file_text.strip():
-        logging.warning("No text extracted from the file.")
+        logger.warning("No text extracted from the file.")
         return {"error": "No text extracted from the file."}
     
     try:
-        logging.info("Initializing Vertex AI with the given project ID and location.")
-        project_id = "genaiq-433814"
+        logger.info("Initializing Vertex AI with the given project ID and location.")
+        project_id = "genaiq"
         location = "us-central1"
-        vertexai.init(project=project_id, location=location)
+        initialize_vertex_ai(project_id, location)
 
-        logging.info("Loading the Vertex AI generative model.")
+        logger.info("Loading the Vertex AI generative model.")
         model = GenerativeModel("gemini-1.5-flash-001",
-        generation_config={"response_mime_type": "application/json"}
-        )
+                                generation_config={"response_mime_type": "application/json"})
 
-        logging.info("Preparing the prompt for Vertex AI.")
+        logger.info("Preparing the prompt for Vertex AI.")
         prompt = (
             "You are an HR expert with extensive experience in evaluating resumes. "
             "Please review the following resume and provide detailed feedback on the candidate's qualifications, achievements, "
             "and potential for career growth. Highlight areas of strength and suggest improvements. Also, provide an ATS match percentage "
-            "for each section of the resume. Structure the feedback with the following fields:\n\n"
-            "1. 'overall_feedback': A summary of the overall quality of the resume.\n"
+            "for the resume. Structure the feedback with the following fields:\n\n"
+            "1. 'overall_feedback': A summary of the overall quality of the resume. \n"
             "2. 'feedback_cards': A list of sections, each containing:\n"
             "   - 'title': The section title (e.g., Professional Experience, Skills and Technologies, etc.).\n"
             "   - 'description': Detailed feedback on the section.\n"
@@ -135,28 +133,27 @@ def call_vertex_ai(file_uri):
             "Return response in JSON format, also remember to be as human as possible. Tone and clarity. Also be personal by using the person's name."
         )
 
-        logging.info("Creating a Part object from the extracted file text.")
+        logger.info("Creating a Part object from the extracted file text.")
         part = Part.from_text(file_text)
 
-        logging.info("Sending the prompt to Vertex AI for content generation.")
+        logger.info("Sending the prompt to Vertex AI for content generation.")
         response = model.generate_content([part, prompt])
         
-        
         if response and response.text:
-            logging.info("Received response from Vertex AI.")
+            logger.info("Received response from Vertex AI.")
             response_text = response.text
             
-            logging.debug(f"Raw response text: {response_text}")
+            logger.debug(f"Raw response text: {response_text}")
 
             try:
-                return json.loads(response.text.strip('```json').strip('```').strip())
+                return json.loads(response_text.strip('```json').strip('```').strip())
             except json.JSONDecodeError as json_err:
-                logging.error(f"Failed to parse JSON response: {json_err}")
+                logger.error(f"Failed to parse JSON response: {json_err}")
                 return {"error": "Failed to parse JSON response.", "raw_response": response_text}
         else:
-            logging.warning("No response received from Vertex AI.")
+            logger.warning("No response received from Vertex AI.")
             return {"error": "No response received from Vertex AI."}
 
     except Exception as e:
-        logging.error(f"Error in call_vertex_ai: {e}")
+        logger.error(f"Error in call_vertex_ai: {e}")
         return {"error": f"Error processing the file: {str(e)}"}
--- backend/app.py
+++ backend/app.py
@@ -1,167 +1,224 @@
-from flask import Flask, render_template, request, jsonify, send_file, session, url_for
+from flask import Flask, render_template, request, jsonify, send_file, session
 from werkzeug.utils import secure_filename
 import os
 import logging
-import pdfkit
+from logging.handlers import RotatingFileHandler
 from ai_service import upload_local_file_to_gcs, call_vertex_ai
 
 app = Flask(__name__)
 
 # Configuration
-UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER', 'temp_uploads/')
-LOG_FILE_PATH = os.getenv('LOG_FILE_PATH', '/home/bamidele_tewogbade1/genaiq/backend/app.log')
+UPLOAD_FOLDER = '/home/bishoptewogbade/genaiq/backend/static/uploads/temp_uploads'
+LOG_FILE_PATH = '/home/bishoptewogbade/genaiq/backend/app.log'
 SECRET_KEY = "bishop"
-PDF_FOLDER = os.getenv('PDF_FOLDER', 'temp_pdfs/')
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['PDF_FOLDER'] = PDF_FOLDER
 app.secret_key = SECRET_KEY
 
-# Set up logging
-logging.basicConfig(level=logging.INFO, 
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    handlers=[logging.FileHandler(LOG_FILE_PATH),
-                              logging.StreamHandler()])
+def setup_logging():
+    """Sets up logging with file rotation."""
+    # Create a file handler for logging with rotation
+    file_handler = RotatingFileHandler(LOG_FILE_PATH, maxBytes=10000000, backupCount=5)
+    file_handler.setLevel(logging.INFO)
 
-logger = logging.getLogger(__name__)
+    # Create a formatter and set it for the handler
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+
+    # Create a logger object
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    logger.addHandler(file_handler)
+
+# Call setup_logging to configure logging
+setup_logging()
 
 def ensure_folder_exists(folder_path):
     """Ensure the folder exists, create it if not."""
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
-        logger.info(f"Created directory: {folder_path}")
+        app.logger.info(f"Created directory: {folder_path}")
 
-# Ensure the upload and PDF directories exist
+# Ensure the upload directory exists
 ensure_folder_exists(app.config['UPLOAD_FOLDER'])
-ensure_folder_exists(app.config['PDF_FOLDER'])
 
 # Allowed file extensions
 ALLOWED_EXTENSIONS = {'pdf', 'docx', 'txt'}
 
 def allowed_file(filename):
+    """Check if the file extension is allowed."""
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def index():
-    logger.info("Rendering index page")
+    app.logger.info("Rendering index page")
     return render_template('index.html')
 
 @app.route('/upload_resume_page')
 def upload_resume_page():
-    logger.info("Rendering upload resume page")
+    app.logger.info("Rendering upload resume page")
     return render_template('upload_resume_index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    logger.info("Received a file upload request")
+    app.logger.info("Received a file upload request")
     
     if 'resume' not in request.files:
-        logger.warning("No file part in request")
+        app.logger.warning("No file part in request")
         return jsonify({'error': 'No file part'}), 400
 
     file = request.files['resume']
     if file.filename == '':
-        logger.warning("No selected file")
+        app.logger.warning("No selected file")
         return jsonify({'error': 'No selected file'}), 400
 
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(filepath)
-        logger.info(f"File {filename} saved to {filepath}")
+        app.logger.info(f"File {filename} saved to {filepath}")
 
         try:
             # Upload file to GCS
-            bucket_name = 'genaiq_storage'
+            bucket_name = 'genaiq_cloudbuild'
             destination_blob_name = f'uploads/{filename}'
             file_uri = upload_local_file_to_gcs(filepath, bucket_name, destination_blob_name)
-            logger.info(f"File URI: {file_uri}")
+            app.logger.info(f"File URI: {file_uri}")
 
             # Store filename and file URI in session
             session['filename'] = filename
             session['file_uri'] = file_uri
-            logger.info(f"Session data after upload: {session}")
+            app.logger.info(f"Session data after upload: {session}")
 
             # Determine file type and prepare response
-            if file.mimetype == 'application/pdf':
-                logger.info(f"File type is PDF")
-                response = {'success': True, 'file_type': 'pdf', 'file_url': filepath}
-            elif file.mimetype == 'text/plain':
+            file_type = file.mimetype.split('/')[1]
+            response = {'success': True, 'file_type': file_type}
+
+            if file_type == 'pdf':
+                response['file_url'] = filepath
+                app.logger.info("File type is PDF")
+            elif file_type == 'plain':
                 with open(filepath, 'r') as f:
                     content = f.read()
-                logger.info("File type is TXT")
-                response = {'success': True, 'file_type': 'txt', 'content': content}
-            elif file.mimetype == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
-                logger.info("File type is DOCX")
-                response = {'success': True, 'file_type': 'docx', 'file_url': filepath}
+                response['content'] = content
+                app.logger.info("File type is TXT")
+            elif file_type == 'vnd.openxmlformats-officedocument.wordprocessingml.document':
+                response['file_url'] = filepath
+                app.logger.info("File type is DOCX")
             else:
-                logger.error("Unsupported file type")
+                app.logger.error("Unsupported file type")
                 response = {'error': 'Unsupported file type'}
 
         except Exception as e:
-            logger.error(f"Error processing file: {e}")
+            app.logger.error(f"Error processing file: {e}")
             response = {'error': 'File processing error'}
 
         finally:
             # Clean up the local file
             if os.path.exists(filepath):
                 os.remove(filepath)
-                logger.info(f"File {filename} removed from local storage")
+                app.logger.info(f"File {filename} removed from local storage")
 
         return jsonify(response), 200 if 'error' not in response else 400
     else:
-        logger.error("File type not allowed")
+        app.logger.error("File type not allowed")
         return jsonify({'error': 'File type not allowed'}), 400
 
 @app.route('/get_feedback_page', methods=['POST'])
 def get_feedback_page():
-    logger.info("Received request to get feedback page")
+    app.logger.info("Received request to get feedback page")
 
     try:
         filename = session.get('filename')
         file_uri = session.get('file_uri')
-        logger.info(f"Session data at feedback page: filename={filename}, file_uri={file_uri}")
+        app.logger.info(f"Session data at feedback page: filename={filename}, file_uri={file_uri}")
 
         if not filename or not file_uri:
-            logger.error("Required session data missing")
+            app.logger.error("Required session data missing")
             return jsonify({'error': 'Session data missing'}), 400
 
         # Process the file using AI service
-        logger.info("Calling AI service to process the file")
+        app.logger.info("Calling AI service to process the file")
+
         try:
-            analysis_result = call_vertex_ai(file_uri)
-            logger.info(f"Analysis result: {analysis_result}")
+            feedback = call_vertex_ai(file_uri)
+            app.logger.info(f"Analysis result: {feedback}")
+
+            # Process the result and prepare feedback data dynamically
+            analysis_result = feedback
+            return render_template('feedback_page.html', feedback=analysis_result), 200
+
         except Exception as e:
-            logger.error(f"Error calling AI service: {e}")
+            app.logger.error(f"Error calling AI service: {e}")
             return jsonify({'error': 'Error calling AI service'}), 500
-        
-        return render_template('feedback_page.html', analysis_result=analysis_result), 200
 
     except Exception as e:
-        logger.error(f"Error in feedback page: {e}")
+        app.logger.error(f"Error processing feedback: {e}")
         return jsonify({'error': 'Error processing feedback'}), 500
+
+def call_vertex_ai_test():
+    """Simulated response from AI service."""
+    simulated_response = {
+        'overall_feedback': 'Sample overall feedback from AI.',
+        'feedback_cards': [
+            {
+                'title': 'Summary',
+                'description': 'Sample description for Summary.',
+                'ats_match': 80,
+                'recommendations': [
+                    'Recommendation 1 for Summary',
+                    'Recommendation 2 for Summary'
+                ]
+            },
+            {
+                'title': 'Experience',
+                'description': 'Sample description for Experience.',
+                'recommendations': [
+                    'Recommendation 1 for Experience',
+                    'Recommendation 2 for Experience'
+                ]
+            },
+            {
+                'title': 'Education',
+                'description': 'Sample description for Education.',
+                'recommendations': [
+                    'Recommendation 1 for Education',
+                    'Recommendation 2 for Education'
+                ]
+            },
+            {
+                'title': 'Skills',
+                'description': 'Sample description for Skills.',
+                'recommendations': [
+                    'Recommendation 1 for Skills',
+                    'Recommendation 2 for Skills'
+                ]
+            },
+        ]
+    }
+    return simulated_response
 
 @app.route('/download_feedback_pdf')
 def download_feedback_pdf():
     """Serve the feedback PDF for download."""
     pdf_filepath = session.get('pdf_filepath')
     if pdf_filepath and os.path.exists(pdf_filepath):
-        logger.info(f"Serving PDF: {pdf_filepath}")
+        app.logger.info(f"Serving PDF: {pdf_filepath}")
         return send_file(pdf_filepath, as_attachment=True)
     else:
-        logger.error("PDF file not found")
+        app.logger.error("PDF file not found")
         return jsonify({'error': 'PDF file not found'}), 404
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    logger.info(f"Serving file {filename}")
+    app.logger.info(f"Serving file {filename}")
     return send_file(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
 @app.route('/tools')
 def tools():
-    logger.info("Rendering tools selection page")
+    app.logger.info("Rendering tools selection page")
     return render_template('tool_selection_page.html')
 
 if __name__ == "__main__":
-    logger.info("Starting Flask application")
-    app.run(debug=os.getenv('FLASK_DEBUG', 'False').lower() == 'true')
+    app.logger.info("Starting Flask application")
+    app.run(debug=True)
