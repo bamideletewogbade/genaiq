8eb2824ec11eedb03e2cc0d3b5bf84edd51ddfab
bamideletewogbade <bishoptewogbade.com>
2024-09-30 01:48:52 +0000

    latest push

--- app.py
+++ app.py
@@ -434,4 +434,4 @@ def download_feedback_pdf():
 
 if __name__ == "__main__":
     app.logger.info("Starting Flask application")
-    app.run(host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=8080)
