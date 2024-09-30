15656ef0edf37f07694742da85b220419a508575
bamideletewogbade <bishoptewogbade.com>
2024-09-30 12:40:04 +0000

    latest build to render full feedback after payment

--- app.py
+++ app.py
@@ -400,8 +400,8 @@ def verify_payment():
 
         if response_data['status']:
             # Payment was successful
-            # feedback = session.get('ai_response')
-            return redirect(url_for('full_report'))
+            feedback = session.get('ai_response')
+            return render_template('full_report.html', feedback=feedback), 200
         else:
             return jsonify({
                 'status': 'failed',
