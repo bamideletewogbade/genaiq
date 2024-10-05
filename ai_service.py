263f5a2f523df660c39c4a71433208be18c8c853
bamideletewogbade <bishoptewogbade.com>
2024-10-05 00:29:38 +0000

    latest push to fix response file from matcher to gcs

--- ai_service.py
+++ ai_service.py
@@ -56,8 +56,8 @@ def save_response_to_file(response, file_path):
         logger.info(f"Response saved successfully to file: {file_path}")
     except Exception as e:
         logger.error(f"Error saving response to file: {e}")
-        
-def save_response_to_gcs(response, file_path, bucket_name, jd_matcher_result_destination_blob_name):
+
+def save_response_to_gcs(response, file_path, bucket_name, destination_blob_name):
     """Saves the AI response to a local file and uploads it to Google Cloud Storage."""
     save_response_to_file(response, file_path)
     gcs_path = upload_local_file_to_gcs(file_path, bucket_name, destination_blob_name)
